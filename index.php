d156d9f0716e791a8f5031210b9997e790ab148a
DamianKolano <Kolano34212@wp.pl>
2018-02-04 13:44:13 +0100

    TO jest klon

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Moj pierwszy projekt</title>
+        <title>Moj pierwszy projekt - klon</title>
     </head>
     <body>
         <?php
